5431066175b642f73f6473c094394c6903c49b3b
Andres_Murillo <134730586+FelipeG-G@users.noreply.github.com>
2025-10-17 15:40:54 -0500

    ajuste en db
    Simplified MongoDB connection options and updated log messages.

--- src/config/database.ts
+++ src/config/database.ts
@@ -7,14 +7,11 @@ const mongoURI = process.env.MONGO_URI; // Obtén el URI desde el archivo .env
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(mongoURI!, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(mongoURI!);
 
-    console.log("Conexión a MongoDB exitosa");
+    console.log("✅ Conexión a MongoDB exitosa");
   } catch (error) {
-    console.error("Error de conexión a MongoDB:", error);
+    console.error("❌ Error de conexión a MongoDB:", error);
     process.exit(1); // Detener el servidor si no se puede conectar a la base de datos
   }
 };
