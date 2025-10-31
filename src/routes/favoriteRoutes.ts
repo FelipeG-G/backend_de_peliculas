bffba5c85dabc1dddaa3f30087c783bf068496af
Andres <pipee886@gmail.com>
2025-10-30 22:00:35 -0500

    ajuste para añadir favorito.v4

--- src/routes/favoriteRoutes.ts
+++ src/routes/favoriteRoutes.ts
@@ -13,7 +13,6 @@ const router = Router();
 //  Obtener todos los favoritos del usuario logueado
 router.get("/", (req, res) => FavoriteController.getUserFavorites(req, res));
 
-router.post("/", (req, res) => FavoriteController.addFavorite(req, res));
 
 //  Agregar un nuevo favorito
 router.post("/", (req, res) => FavoriteController.addFavorite(req, res));
